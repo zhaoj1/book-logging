45d1fd19f81b2da4d5b9a9557cfbb7e87e66357d
github username <justinzhao265@gmail.com>
2020-08-28 18:12:55 -0400

    Updates

---
+++ precache-manifest.7e9dd9f00cb937c383af4f040bcfad5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e862745a2c3a56ae467b26ddf0f67c6",
+    "url": "/book-logging/index.html"
+  },
+  {
+    "revision": "2dcf8dfd0e0160e6775f",
+    "url": "/book-logging/static/css/main.0c0836ef.chunk.css"
+  },
+  {
+    "revision": "3d2593375bfd62ffbbe7",
+    "url": "/book-logging/static/js/2.25654e7d.chunk.js"
+  },
+  {
+    "revision": "d4a04d94a4159612a3f050c041734671",
+    "url": "/book-logging/static/js/2.25654e7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dcf8dfd0e0160e6775f",
+    "url": "/book-logging/static/js/main.e200e378.chunk.js"
+  },
+  {
+    "revision": "e4a01fc65bb79d4761a5",
+    "url": "/book-logging/static/js/runtime-main.1cf330ed.js"
+  },
+  {
+    "revision": "34e08fae047b792c4f594ed13ddfe59f",
+    "url": "/book-logging/static/media/background.34e08fae.png"
+  }
+]);
