989e1f372b09182386fba510cbd4354631c0eace
github username <justinzhao265@gmail.com>
2020-08-29 13:52:19 -0400

    adjust to hashrouter

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Modal from 'react-modal'
 
 import Landing from './Landing';
