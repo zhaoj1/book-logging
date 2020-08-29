3eeb60ee6cf25bbf926140764907d86c8e55ea2d
github username <justinzhao265@gmail.com>
2020-08-29 13:52:44 -0400

    Updates

--- precache-manifest.b17d4889a3dfa59206059d7c3432ec90.js
+++ precache-manifest.b17d4889a3dfa59206059d7c3432ec90.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8e862745a2c3a56ae467b26ddf0f67c6",
+    "revision": "fd777103be5ef68add577c8281b76b08",
     "url": "/book-logging/index.html"
   },
   {
-    "revision": "2dcf8dfd0e0160e6775f",
+    "revision": "78dfe7311bdfd8071ee0",
     "url": "/book-logging/static/css/main.0c0836ef.chunk.css"
   },
   {
-    "revision": "3d2593375bfd62ffbbe7",
-    "url": "/book-logging/static/js/2.25654e7d.chunk.js"
+    "revision": "30ed4a2f9859f5f9c19a",
+    "url": "/book-logging/static/js/2.0272f59a.chunk.js"
   },
   {
     "revision": "d4a04d94a4159612a3f050c041734671",
-    "url": "/book-logging/static/js/2.25654e7d.chunk.js.LICENSE.txt"
+    "url": "/book-logging/static/js/2.0272f59a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2dcf8dfd0e0160e6775f",
-    "url": "/book-logging/static/js/main.e200e378.chunk.js"
+    "revision": "78dfe7311bdfd8071ee0",
+    "url": "/book-logging/static/js/main.9820db85.chunk.js"
   },
   {
     "revision": "e4a01fc65bb79d4761a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/book-logging/precache-manifest.7e9dd9f00cb937c383af4f040bcfad5c.js"
+  "/book-logging/precache-manifest.b17d4889a3dfa59206059d7c3432ec90.js"
 );
 
 self.addEventListener('message', (event) => {
