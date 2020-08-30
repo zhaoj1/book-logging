7cf9f46f70953dccc87c19b3f4f800948c655fc3
github username <justinzhao265@gmail.com>
2020-08-30 15:36:55 -0400

    loading

--- src/components/Search.js
+++ src/components/Search.js
@@ -26,7 +26,6 @@ export default class Search extends React.Component{
 
   searchAPI = async (event) => {
     event.preventDefault();
-    this.props.toggleLoading()
     const resp = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.state.queryParams}+author:${this.state.authorQuery}&maxResults=40&key=` + process.env.REACT_APP_GOOGLE_BOOKS_API_KEY)
     const json = await resp.json()
     if(json.totalItems == 0){
@@ -37,7 +36,6 @@ export default class Search extends React.Component{
         this.props.setDefaultAuthor(this.state.authorQuery)
       }
       this.props.setResults(json)
-      this.props.toggleLoading()
       this.setState({searched: true})
     }
   }
